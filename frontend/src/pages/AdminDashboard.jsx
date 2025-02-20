864541273f329401d6e20e30dc559657b671a693
AkshayaNagaraj <cnagaraj332@gmail.com>
2025-02-20 09:44:01 +0530

    Design for bookinglist in admin done

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -5,22 +5,31 @@ import AddService from "./AddService";
 function AdminDashboard() {
   return (
     <div>
-      <h1>Admin Dashboard</h1>
-      <div>
-        <h2>Bookings</h2>
-        <BookingList />
+      <div className="flex flex-col items-center  justify-center p-3 font-avr">
+        <h1 className="text-customBrown font-avr tracking-widest text-xl text-center items-center p-10 sm:text-xl md:text-2xl hover:underline hover:scale-105 transition duration-300 ease-in-out">
+          Admin Dashboard
+        </h1>
+        <div className="text-xl">
+          <p>
+            Manage your bookings, portfolio, and
+            <p> clients effortlessly in one place. Stay </p>
+            <p>organized, track your progress, and provide</p>
+            <p>the best experience for your clients."</p>
+          </p>
+        </div>
       </div>
+
       <div>
-        <h2>Manage Portfolio</h2>
         <AddPortfolio />
       </div>
       <div>
-        <h2>Add services</h2>
         <AddService></AddService>
       </div>
-      
+      <div>
+        <BookingList />
+      </div>
     </div>
   );
-};
+}
 
 export default AdminDashboard;
--- frontend/src/pages/BookList.jsx
+++ frontend/src/pages/BookList.jsx
@@ -22,32 +22,34 @@ function BookingList() {
   }, []);
 
   return (
-    <div>
-      <h1>Bookings</h1>
-      <table>
-        <thead>
+    <div className="p-4 mb-10">
+      <h1 className="text-customBrown font-avr tracking-widest text-xl text-center items-center p-10 sm:text-xl md:text-2xl hover:underline hover:scale-105 transition duration-300 ease-in-out">Bookings</h1>
+      <div className=" overflow-x-auto shadow-lg rounded-lg  sm:text-sm md:text-md lg:text-lg">
+      <table className=" w-full border-collapse border border-gray-700 mb-10">
+        <thead className="text-customBrown text-lg px-3 hover:bg-customBrown transition delay-75 hover:text-white">
           <tr>
-            <th>Client</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Service</th>
-            <th>Date</th>
+            <th className="border px-5 py-2">Client</th>
+            <th className="border px-5 py-2">Email</th>
+            <th className="border px-5 py-2">Phone</th>
+            <th className="border px-5 py-2">Service</th>
+            <th className="border px-5 py-2">Date</th>
             
           </tr>
         </thead>
         <tbody>
           {bookings.map((booking) => (
-            <tr key={booking._id}>
-              <td>{booking.clientName}</td>
-              <td>{booking.email}</td>
-              <td>{booking.phone}</td>
-              <td>{booking.service}</td>
-              <td>{new Date(booking.date).toLocaleString()}</td>
+            <tr className="hover:bg-customBeige transition delay-150"key={booking._id}>
+              <td className="border px-5 py-2">{booking.clientName}</td>
+              <td className="border px-5 py-2">{booking.email}</td>
+              <td className="border px-5 py-2">{booking.phone}</td>
+              <td className="border px-5 py-2">{booking.service}</td>
+              <td className="border px-5 py-2">{new Date(booking.date).toLocaleString()}</td>
               
             </tr>
           ))}
         </tbody>
       </table>
+      </div>
     </div>
   );
 }
